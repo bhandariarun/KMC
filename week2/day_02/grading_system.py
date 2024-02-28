d37696d8b1f88d75b7b86041e1c764dbf1f6fc23
arun <abbhan053@gmail.com>
2024-02-29 00:13:53 +0545

    Day2 work in condition and branching

--- week2/day_02/grading_system.py
+++ week2/day_02/grading_system.py
@@ -1,3 +1,20 @@
+# # Task 01: Grading System Exercise
+#
+# ## Objective
+# Create a simple grading system where a student's score is entered, and the program determines the corresponding grade.
+#
+# ## Requirements
+#
+# 1. Get the student's score as input.
+# 2. Use the following grading scale:
+#    - 90-100: A
+#    - 80-89: B
+#    - 70-79: C
+#    - 60-69: D
+#    - Below 60: F
+# 3. Print the grade based on the input score.
+# 4. Handle cases where the entered score is not within the valid range (0-100).
+
 def student(score):
     if score >=90 and score <= 100:
         print("The marks secured by student is 90-100:A")
