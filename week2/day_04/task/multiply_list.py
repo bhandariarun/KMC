23fcf95326f5a954975689cd3601971cc68bd4de
arun <abbhan053@gmail.com>
2024-02-28 20:50:41 +0545

    Day4  works in arguments and functions

---
+++ week2/day_04/task/multiply_list.py
@@ -0,0 +1,9 @@
+#Write a python function to multiply all the members in a list
+
+def multiply(listed):
+    result = 1
+    for x in listed:
+        result= result * x
+    return result
+list1 = [1,2,3,4,5]
+print(multiply(list1))
