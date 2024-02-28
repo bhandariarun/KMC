a3858af074908a24cc22c0798505d64db8c2d913
arun <abbhan053@gmail.com>
2024-02-28 23:33:24 +0545

    Day3 works in Loops

--- week2/day_03/number_guessing.py
+++ week2/day_03/number_guessing.py
@@ -1,6 +1,46 @@
+# # Task 01: Word Pyramid Generator
+#
+# ## Task
+#
+# Create a program that generates a word pyramid pattern based on user input.
+#
+# ## Objective
+#
+# The objective is to generate and print a pyramid pattern using the letters of the word provided by the user. Each level of the pyramid should display the letters of the word up to that level, and the word should be centered on each level of the pyramid.
+#
+# ## Requirements
+#
+# 1. Ask the user to input a word.
+# 2. Generate and print a pyramid pattern using the letters of the word.
+# 3. Each level of the pyramid should display the letters of the word up to that level.
+# 4. The word should be centered on each level of the pyramid.
+#
+# ## Additional Challenges
+#
+# 1. Implement a function to validate the input and ensure it's a valid word.
+# 2. Allow the user to choose the direction of the pyramid (upwards or downwards).
+# 3. Enhance the program to handle phrases or sentences instead of single words.
+#
+# Expected Output:
+# if word level is up:
+# ```
+#             S
+#            S u
+#           S u n
+#          S u n i
+#         S u n i l
+# ```
+#
+# if word level is Down:
+# ```
+#             S u n i l
+#              S u n i
+#               S u n
+#                S u
+#                 S
+# ```
+
 import random
-
-
 def number_guessing_game():
     print("Welcome to the Number Guessing Game!")
     print("-------------------------------")
