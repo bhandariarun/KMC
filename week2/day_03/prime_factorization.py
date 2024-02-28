0427db799b8449dda118e0693a5deedec190ff6d
arun <abbhan053@gmail.com>
2024-02-29 00:00:54 +0545

    Day3 works in Loops

--- week2/day_03/prime_factorization.py
+++ week2/day_03/prime_factorization.py
@@ -1,3 +1,20 @@
+# # Task 03: Prime Factorization
+#
+# ## Objective
+#
+# Create a program that takes an integer input from the user and prints its prime factorization.
+# 
+# ## Requirements
+#
+# 1. Ask the user to input a positive integer.
+# 2. Compute and print the prime factorization of the input integer.
+#
+# ## Additional Challenges
+#
+# 1. Implement error handling to ensure the user inputs a valid positive integer.
+# 2. Allow the program to handle edge cases, such as the input being 1 or a prime number.
+# 3. Enhance the program to handle multiple integer inputs in a loop until the user chooses to exit.
+
 def sort_numbers(input_list, ascending=True):
     odd_numbers = []
     even_numbers = []
