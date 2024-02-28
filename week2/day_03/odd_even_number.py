ebf5915581f054a6c445f9ead407aa78c065ef04
arun <abbhan053@gmail.com>
2024-02-28 23:34:35 +0545

    Day3 works in Loops

--- week2/day_03/odd_even_number.py
+++ week2/day_03/odd_even_number.py
@@ -1,3 +1,20 @@
+# # Task 02: List Manipulation - Odd-Even Sorter
+# 
+# ## Objective
+#
+# Create a program that takes a list of numbers from the user, sorts them into two separate lists (one for odd numbers and one for even numbers), and displays the sorted lists.
+#
+# ## Requirements
+#
+# 1. Ask the user to input a list of numbers (comma-separated).
+# 2. Sort the numbers into two lists: one for odd numbers and one for even numbers.
+# 3. Display both lists.
+#
+# ## Additional Challenges
+#
+# 1. Allow the user to input any type of values (not just numbers) and handle different data types.
+# 2. Enhance the program to display the sorted lists in ascending or descending order.
+
 def sort_numbers(input_list, ascending=True):
     odd_numbers = []
     even_numbers = []
