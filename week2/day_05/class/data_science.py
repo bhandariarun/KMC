040cfb1aec737b411e3ad63f4a668fcf9932df73
arun <abbhan053@gmail.com>
2024-02-29 18:17:47 +0545

    Day5 is about class and methods

--- week2/day_05/class/data_science.py
+++ week2/day_05/class/data_science.py
@@ -4,24 +4,21 @@ Example:
 If class is a entity then attributes are
 """
 
-
 class Car():
     def __init__(self, name, fuel_type):
         self.name = name
         self.fuel_type = fuel_type
 
-    def display(self):
-        print(self.name, self.model, self.speeds)
 class Maruti(Car):
     def __init__(self, name, fuel_type, speeds):
         super().__init__(name, fuel_type)
         self.speeds = speeds
 
     def travel(self):
-        print(f"{self.name}:{self.speeds}")
+        print(f"{self.name} : {self.speeds}")
 
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     obj = Maruti('Suzuki','Petrol','200KM/HR')
     obj.travel()
